f1592017fd728f8468664ae114060f7efd15f6e0
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-05-31 13:30:40 -0700

    Decide and display winner in div

--- script.js
+++ script.js
@@ -41,19 +41,55 @@ let playRound = (playerSelection, computerSelection) => {
     return result;
 };
 
+let getResult = (result) => {
+    let fields = result.split(' ');
+    return fields[1];
+};
+
+let keepScore = (result) => {
+    if (result === 'Win!') playerScore++;
+    if (result === 'Lose!') compScore++;
+};
+
+let decideWinner = (playerScore, compScore) => {
+    if (playerScore > compScore) winner = 'You win!';
+    else if (compScore > playerScore) winner = `You lose...`;
+    else winner = `You've tied lol`;
+    return winner;
+};
+
+let result = '';
+let compScore = 0;
+let playerScore = 0;
+
 const buttons = document.querySelectorAll('button');
+const container = document.querySelector('.container');
+const div = document.createElement('div');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.textContent, computerPlay()));
+        result = playRound(button.textContent, computerPlay());
+        result = getResult(result);
+        keepScore(result);
+
+        console.log(`Player score: ${playerScore}`);
+        console.log(`Computer score: ${compScore}`);
+        
+        if(playerScore >= 5 || compScore >= 5) {
+            div.textContent = decideWinner(playerScore, compScore);
+            container.appendChild(div);
+        }
+
     });
 });
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
-div.textContent = 'Results';
 
-container.appendChild(div);
+
+
+
+
+
+
 
 
 
